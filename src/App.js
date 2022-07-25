a3baecb684fd0182eeb3489db05b04b5d79aa90e
jane <janneth.llanos@insteclrg.edu.ec>
2022-07-24 23:30:40 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-import './App.css';
-import React ,{ Component } from 'react';
-import { ProvinciaService } from './service/ProvinciaService';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import Provincias from "./components/provincia";
+//import { Actualizar, Crear, Eliminar } from "./pages";
 
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <div className="bg-slate-400 h-screen p-2 flex flex-col items-center ">
+          <header className="flex justify-between w-[95vw] items-center bg-white right-0 left-0 mx-auto rounded-md p-3 h-[5rem] shadow-2xl">
+            <NavLink to={"/"}>
+              <div className="bg-slate-400 p-2 rounded-lg shadow-md text-slate-200 hover:bg-orange-300">
+                VOY A LLORAR
+              </div>
+            </NavLink>
+            <img src={logo} className="App-logo h-[5rem]" alt="logo" />
+          </header>
 
+          <div className="mt-10 w-screen lg:w-[50rem] p-2">
+            <Routes>
+              <Route path="/" element={<Provincias />} />
+           
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
 
-
-export default class App extends Component {
-  constructor() {
-  super();
-  this.state={};
-  this.ProvinciaService= new ProvinciaService();
- }
-  
-  componentDidMount() {
-  this.ProvinciaService.getAll().then(data=>{  
-    console.log(data);
-   }  );
-  
-  }
-  render( ){  
-  return( 
-<p> :   voy a llorar</p>
-   
-
-
-  
-    );
-  
-  }
-
-  }
-
-  
+export default App;
